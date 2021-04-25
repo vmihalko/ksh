2aad3cab06dbc28645bb2df4bcea4bfd4d8bfec4
Martijn Dekker <martijn@inlv.org>
2021-04-25 23:52:40 +0100

    Add ksh 93u+m contributors notice to 964 copyright headers

--- src/cmd/INIT/m.c
+++ src/cmd/INIT/m.c
@@ -2,6 +2,7 @@
 *                                                                      *
 *               This software is part of the ast package               *
 *          Copyright (c) 1994-2011 AT&T Intellectual Property          *
+*          Copyright (c) 2020-2021 Contributors to ksh 93u+m           *
 *                      and is licensed under the                       *
 *                 Eclipse Public License, Version 1.0                  *
 *                    by AT&T Intellectual Property                     *
