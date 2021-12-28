0e197eee57041b13ef7227a71335e8d113e6e54a
Johnothan King <johnothanking@protonmail.com>
2021-12-28 08:53:30 -0800

    Fix `mkservice` compile errors and add `SHOPT_MKSERVICE` (#401)
    The unused mkservice and eloop builtins are currently not built, and if
    an attempt to compile them is made the build ends in failure. This
    commit backports a few build fixes from ksh93v- 2012-08-24 that allow
    mkservice and eloop to build (plus an additional compiler warning fix
    not in ksh93v-). I've also added a new SHOPT_MKSERVICE setting (turned
    off by default) so that mkservice and eloop can be built if the user
    chooses to include them in their build of ksh.

--- src/cmd/ksh93/data/builtins.c
+++ src/cmd/ksh93/data/builtins.c
@@ -114,6 +114,10 @@ const struct shtable3 shtab_builtins[] =
 #endif	/* JOBS */
 	"false",	NV_BLTIN|BLT_ENV,		bltin(false),
 	"getopts",	NV_BLTIN|BLT_ENV,		bltin(getopts),
+#if SHOPT_MKSERVICE
+	"mkservice",	NV_BLTIN|BLT_ENV,		bltin(mkservice),
+	"eloop",	NV_BLTIN|BLT_ENV,		bltin(eloop),
+#endif /* SHOPT_MKSERVICE */
 	"print",	NV_BLTIN|BLT_ENV,		bltin(print),
 	"printf",	NV_BLTIN|BLT_ENV,		bltin(printf),
 	"pwd",		NV_BLTIN,			bltin(pwd),
