9c313c7fe3686fab56b786a877b3ae507869bbff
Martijn Dekker <martijn@inlv.org>
2022-01-30 20:37:09 +0000

    Update copyright years in files changed since 1st Jan 2022

--- src/lib/libcmd/rm.c
+++ src/lib/libcmd/rm.c
@@ -2,7 +2,7 @@
 *                                                                      *
 *               This software is part of the ast package               *
 *          Copyright (c) 1992-2013 AT&T Intellectual Property          *
-*          Copyright (c) 2020-2021 Contributors to ksh 93u+m           *
+*          Copyright (c) 2020-2022 Contributors to ksh 93u+m           *
 *                      and is licensed under the                       *
 *                 Eclipse Public License, Version 1.0                  *
 *                    by AT&T Intellectual Property                     *
