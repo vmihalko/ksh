vmihalko/ksh

commit 0863a8eb298729f746ee4a0fcbb041d4a017f2ae

Support glibc 2.35's posix_spawn_file_actions_addtcsetpgrp_np(3)
This commit implements support for the glibc 2.35
posix_spawn_file_actions_addtcsetpgrp_np(3) extension[2][3],
updating spawnveg(3) to use the new function for setting the
terminal group. This was done with the intention of improving
performance in interactive shells without reintroducing previous
race conditions[4][5].

[1]: https://sourceware.org/pipermail/libc-alpha/2022-February/136040.html
[2]: https://sourceware.org/git/?p=glibc.git;a=commit;h=342cc934
[3]: https://sourceware.org/git/?p=glibc.git;a=commit;h=6289d28d
[4]: https://github.com/ksh93/ksh/issues/79
[5]: https://www.mail-archive.com/ast-developers@research.att.com/msg00717.html

src/cmd/ksh93/sh/path.c:
- Tell spawnveg(3) to set the terminal process group when launching
  a child process in an interactive shell.

src/cmd/ksh93/sh/xec.c:
- If posix_spawn_file_actions_addtcsetpgrp_np(3) is available,
  allow use of spawnveg(3) (via sh_ntfork()) even with job control
  active.
- sh_ntfork(): Reimplement most of the SIGTSTP handling code
  removed in commit 66c37202.

src/lib/libast/comp/spawnveg.c,
src/lib/libast/misc/procopen.c,
src/lib/libast/features/sys:
- Add support for posix_spawn_file_actions_addtcsetpgrp_np(3).
- Allow spawnveg to set the terminal process group when pgid == 0.
  This was necessary to avoid race conditions when using the new
  function.

src/lib/libast/features/lib:
- Detect posix_spawn_file_actions_addtcsetpgrp_np(3).
- Do not detect an OS spawnveg(3). With the API changes to spawnveg
  in this pull request ksh probably can't use the OS's spawnveg
  function anymore. (That's assuming anything else even provides a
  spawnveg function to begin with, which is unlikely.)

src/lib/libast/features/api,
src/cmd/ksh93/include/defs.h:
- Bump libast version (20220101 => 20220201) due to the spawnveg(3)
  API change.

src/lib/libast/man/spawnveg.3:
- Document the changes to spawnveg(3) in the corresponding man

src/cmd/ksh93/sh/xec.c
@@ -55,6 +55,10 @@
 #   include <sys/resource.h>
 #endif
 
+#if _lib_posix_spawn > 1 && _lib_posix_spawn_file_actions_addtcsetpgrp_np
+#define _use_ntfork_tcpgrp 1
+#endif
+
 #define SH_NTFORK	SH_TIMING
 #define NV_BLTPFSH	NV_ARRAY
 
@@ -1613,7 +1617,11 @@ int sh_exec(register const Shnode_t *t, int flags)
 					fifo_save_ppid = sh.current_pid;
 #endif
 #if SHOPT_SPAWN
+#if _use_ntfork_tcpgrp
+				if(com)
+#else
 				if(com && !job.jobcontrol)
+#endif /* _use_ntfork_tcpgrp */
 				{
 					parent = sh_ntfork(t,com,&jobid,ntflag);
 					if(parent<0)
@@ -3449,7 +3457,8 @@ static void sigreset(int mode)
 
 /*
  * A combined fork/exec for systems with slow fork().
- * Incompatible with job control on interactive shells (job.jobcontrol).
+ * Incompatible with job control on interactive shells (job.jobcontrol) if
+ * the system does not support posix_spawn_file_actions_addtcsetpgrp_np().
  */
 static pid_t sh_ntfork(const Shnode_t *t,char *argv[],int *jobid,int flag)
 {
@@ -3461,6 +3470,9 @@ static pid_t sh_ntfork(const Shnode_t *t,char *argv[],int *jobid,int flag)
 	char		**arge, *path;
 	volatile pid_t	grp = 0;
 	Pathcomp_t	*pp;
+#if _use_ntfork_tcpgrp
+	volatile int	jobwasset=0;
+#endif /* _use_ntfork_tcpgrp */
 	if(flag)
 	{
 		otype = savetype;
@@ -3525,8 +3537,21 @@ static pid_t sh_ntfork(const Shnode_t *t,char *argv[],int *jobid,int flag)
 		}
 		arge = sh_envgen();
 		sh.exitval = 0;
+#if _use_ntfork_tcpgrp
+		if(job.jobcontrol)
+		{
+			signal(SIGTTIN,SIG_DFL);
+			signal(SIGTTOU,SIG_DFL);
+			signal(SIGTSTP,SIG_DFL);
+			jobwasset++;
+		}
+#endif /* _use_ntfork_tcpgrp */
 #ifdef JOBS
+#if _use_ntfork_tcpgrp
+		if(sh_isstate(SH_MONITOR) && (job.jobcontrol || (otype&FAMP)))
+#else
 		if(sh_isstate(SH_MONITOR) && (otype&FAMP))
+#endif /* _use_ntfork_tcpgrp */
 		{
 			if((otype&FAMP) || job.curpgid==0)
 				grp = 1;
@@ -3587,6 +3612,17 @@ static pid_t sh_ntfork(const Shnode_t *t,char *argv[],int *jobid,int flag)
 	sh_popcontext(buffp);
 	if(buffp->olist)
 		free_list(buffp->olist);
+#if _use_ntfork_tcpgrp
+	if(jobwasset)
+	{
+		signal(SIGTTIN,SIG_IGN);
+		signal(SIGTTOU,SIG_IGN);
+		if(sh_isstate(SH_INTERACTIVE))
+			signal(SIGTSTP,SIG_IGN);
+		else
+			signal(SIGTSTP,SIG_DFL);
+	}
+#endif /* _use_ntfork_tcpgrp */
 	if(sigwasset)
 		sigreset(1);	/* restore ignored signals */
 	if(scope)

src/cmd/ksh93/include/defs.h
@@ -29,8 +29,8 @@
 #define defs_h_defined
 
 #include	<ast.h>
-#if !defined(AST_VERSION) || AST_VERSION < 20220101
-#error libast version 20220101 or later is required
+#if !defined(AST_VERSION) || AST_VERSION < 20220201
+#error libast version 20220201 or later is required
 #endif
 #if !_lib_fork
 #error In 2021, ksh joined the 21st century and started requiring fork(2).

src/cmd/ksh93/sh/path.c
@@ -147,7 +147,7 @@ static pid_t _spawnveg(const char *path, char* const argv[], char* const envp[],
 	while(1)
 	{
 		sh_stats(STAT_SPAWN);
-		pid = spawnveg(path,argv,envp,pgid);
+		pid = spawnveg(path,argv,envp,pgid,job.jobcontrol?job.fd:-1);
 		if(pid>=0 || errno!=EAGAIN)
 			break;
 	}

src/lib/libast/comp/spawnveg.c
@@ -32,12 +32,6 @@
 
 #include <ast.h>
 
-#if _lib_spawnveg
-
-NoN(spawnveg)
-
-#else
-
 #if _lib_posix_spawn > 1	/* reports underlying exec() errors */
 
 #include <spawn.h>
@@ -45,20 +39,22 @@ NoN(spawnveg)
 #include <wait.h>
 
 pid_t
-spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid)
+spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid, int tcfd)
 {
-	int			err, flags = 0;
-	pid_t			pid;
-	posix_spawnattr_t	attr;
+	int				err, flags = 0;
+	pid_t				pid;
+	posix_spawnattr_t		attr;
+#if _lib_posix_spawn_file_actions_addtcsetpgrp_np
+	posix_spawn_file_actions_t	actions;
+#endif
 
 	if (err = posix_spawnattr_init(&attr))
 		goto nope;
 #if POSIX_SPAWN_SETSID
 	if (pgid == -1)
 		flags |= POSIX_SPAWN_SETSID;
-	else
 #endif
-	if (pgid)
+	if (pgid && pgid != -1)
 		flags |= POSIX_SPAWN_SETPGROUP;
 	if (flags && (err = posix_spawnattr_setflags(&attr, flags)))
 		goto bad;
@@ -69,13 +65,33 @@ spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid)
 		if (err = posix_spawnattr_setpgroup(&attr, pgid))
 			goto bad;
 	}
+#if _lib_posix_spawn_file_actions_addtcsetpgrp_np
+	if (tcfd >= 0)
+	{
+		if (err = posix_spawn_file_actions_init(&actions))
+			goto bad;
+		if (err = posix_spawn_file_actions_addtcsetpgrp_np(&actions, tcfd))
+			goto fail;
+	}
+	if (err = posix_spawn(&pid, path, (tcfd >= 0) ? &actions : NiL, &attr, argv, envv ? envv : environ))
+#else
 	if (err = posix_spawn(&pid, path, NiL, &attr, argv, envv ? envv : environ))
+#endif
 	{
 		if ((err != EPERM) || (err = posix_spawn(&pid, path, NiL, NiL, argv, envv ? envv : environ)))
-			goto bad;
+			goto fail;
 	}
+#if _lib_posix_spawn_file_actions_addtcsetpgrp_np
+	if (tcfd >= 0)
+		posix_spawn_file_actions_destroy(&actions);
+#endif
 	posix_spawnattr_destroy(&attr);
 	return pid;
+ fail:
+#if _lib_posix_spawn_file_actions_addtcsetpgrp_np
+	if (tcfd >= 0)
+		posix_spawn_file_actions_destroy(&actions);
+#endif
  bad:
 	posix_spawnattr_destroy(&attr);
  nope:
@@ -97,8 +113,9 @@ spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid)
 #endif
 
 pid_t
-spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid)
+spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid, int tcfd)
 {
+	NOT_USED(tcfd);
 #if defined(P_DETACH)
 	return spawnve(pgid ? P_DETACH : P_NOWAIT, path, argv, envv ? envv : environ);
 #else
@@ -117,10 +134,11 @@ spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid)
  */
 
 pid_t
-spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid)
+spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid, int tcfd)
 {
 	struct inheritance	inherit;
 
+	NOT_USED(tcfd);
 	inherit.flags = 0;
 	if (pgid)
 	{
@@ -154,7 +172,7 @@ spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid)
  */
 
 pid_t
-spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid)
+spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid, int tcfd)
 {
 #if _lib_fork || _lib_vfork
 	int			n;
@@ -169,6 +187,7 @@ spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid)
 #endif
 #endif
 
+	NOT_USED(tcfd);
 	if (!envv)
 		envv = environ;
 #if _lib_spawnve
@@ -214,11 +233,9 @@ spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid)
 #if _lib_tcgetpgrp
 			if (m)
 				tcsetpgrp(2, pgid);
-#else
-#ifdef TIOCSPGRP
+#elif defined(TIOCSPGRP)
 			if (m)
 				ioctl(2, TIOCSPGRP, &pgid);
-#endif
 #endif
 		}
 		execve(path, argv, envv);
@@ -283,8 +300,6 @@ spawnveg(const char* path, char* const argv[], char* const envv[], pid_t pgid)
 	return -1;
 #endif
 }
-
-#endif
 
 #endif
 

src/lib/libast/features/api
@@ -1,6 +1,6 @@
 iff AST_API
 
-ver ast 20220101
+ver ast 20220201
 
 api ast 20120528 regexec regnexec regrexec regsubexec strgrpmatch
 

src/lib/libast/features/lib
@@ -36,7 +36,7 @@ lib	mount,onexit,opendir,pathconf
 lib	readlink,remove,rename,rewinddir,rindex,rmdir,setlocale
 lib	setpgid,setpgrp,setpgrp2,setreuid,setsid,setuid,sigaction
 lib	sigprocmask,sigsetmask,sigunblock,sigvec,socketpair
-lib	spawn,spawnve,spawnveg
+lib	spawn,spawnve
 lib	strchr,strcoll,strdup,strerror,strcasecmp,strncasecmp,strrchr,strstr
 lib	strmode,strxfrm,strftime,swab,symlink,sysconf,sysinfo,syslog
 lib	telldir,tmpnam,tzset,universe,unlink,utime,wctype
@@ -343,6 +343,7 @@ tst	lib_posix_spawn unistd.h stdlib.h spawn.h -Dfork=______fork note{ posix_spaw
 		_exit(n);
  	}
 }end
+lib	posix_spawn_file_actions_addtcsetpgrp_np
 
 tst	lib_spawn_mode unistd.h stdlib.h note{ first spawn arg is mode and it works }end execute{
 	#include <signal.h>
@@ -642,7 +643,7 @@ tst	- output{
 		printf("\n");
 	#endif
 
-	#if _lib_spawnveg || _lib_posix_spawn || _lib_spawn_mode || _lib_spawn && _hdr_spawn && _mem_pgroup_inheritance || _lib_vfork && _real_vfork
+	#if _lib_posix_spawn || _lib_spawn_mode || _lib_spawn && _hdr_spawn && _mem_pgroup_inheritance || _lib_vfork && _real_vfork
 		printf("#if !_AST_no_spawnveg\n");
 		printf("#define _use_spawnveg	1\n");
 		printf("#endif\n");

src/lib/libast/features/sys
@@ -195,7 +195,7 @@ extern	setpgid		int		(pid_t, pid_t)
 extern	setsid		pid_t		(void)
 extern	setuid		int		(uid_t)
 extern	sleep		unsigned		(unsigned int)
-extern	spawnveg	pid_t		(const char*, char* const[], char* const[], pid_t)
+extern	spawnveg	pid_t		(const char*, char* const[], char* const[], pid_t, int)
 extern	srand		void		(unsigned int)
 extern	strcasecmp	int		(const char*, const char*)
 extern	strcat		char*		(char*, const char*)

src/lib/libast/man/spawnveg.3
@@ -42,7 +42,7 @@ spawnveg \- process spawn with process group and session control
 .SH SYNOPSIS
 .L "#include <ast.h>"
 .sp
-.L "int spawnveg(const char* command, char** argv, char** envv, pid_t pgid);"
+.L "int spawnveg(const char* command, char** argv, char** envv, pid_t pgid, int tcfd);"
 .SH DESCRIPTION
 .L spawnveg
 combines
@@ -81,10 +81,24 @@ The new process becomes a process group leader.
 .L >1
 The new process joins the process group
 .IR pgid .
-.SH CAVEATS
+.PP
 The
+.L tcfd
+argument is currently only used if the operating system supports the
+.I posix_spawn_file_actions_addtcsetpgrp_np
+function.
+When
+.L tcfd
+is
+.LR >=0 ,
+spawnveg will set the controlling terminal for the new process to
+.IR tcfd .
+.SH CAVEATS
+If the
+.I posix_spawn_file_actions_addtcsetpgrp_np
+function is not available, then
 .L spawnveg
-function cannot set the terminal process group.
+cannot reliably set the terminal process group.
 As a result, it is incompatible with job control when used with terminals.
 Additionally, if the
 .L POSIX_SPAWN_SETSID

commit bb3527aea558cbded31c7fccaa3e0c1c5389aa0e

Fix infinite loop when posix_spawn fails (re: 0863a8eb) (#468)
This commit fixes an infinite loop introduced in commit 0863a8eb
that caused ksh to enter an infinite loop if posix_spawn failed
to start a new process after setting the terminal process group.
Reproducer (warning: it will cause ksh to crash Wayland sessions
and drives up CPU usage by a ton):

   $ /tmp/this/file/does/not/exist
   /usr/bin/ksh: /tmp/this/file/does/not/exist: not found
   $ <Press enter>
   (ksh now prints $PS1 in a loop until killed with SIGKILL)

The first bug fixed is the infinite loop that occurs when
posix_spawn fails to execute a command. This was fixed by setting
the terminal process group to the main interactive shell.

The second bug fixed is related to the signal handling of the
SIGTTIN, SIGTTOU and SIGTSTP signals. In sh_ntfork() these signals
are set to their default signal handlers (SIG_DFL) before running
a command. The signal handlers were only restored to SIG_IGN
(ignore signal) when sh_ntfork() successfully ran a command.
This could cause a SIGTTOU lockup under strace when a command
failed to execute in an interactive shell, while also being one
cause of the infinite loop.

src/cmd/ksh93/sh/xec.c: sh_ntfork():
- Restore the terminal process group if posix_spawn failed to
  launch a new process. This is necessary because posix_spawn will
  set the terminal process group before it attempts to run a
  command and doesn't restore it on failure.

src/cmd/ksh93/sh/xec.c
@@ -3598,19 +3598,35 @@ static pid_t sh_ntfork(const Shnode_t *t,char *argv[],int *jobid,int flag)
 	fail:
 		if(jobfork && spawnpid<0) 
 			job_fork(-2);
-		if(spawnpid == -1) switch(errno=sh.path_err)
+		if(spawnpid == -1)
 		{
-		    case ENOENT:
-			errormsg(SH_DICT,ERROR_exit(ERROR_NOENT),e_found+4);
-			UNREACHABLE();
+#if _use_ntfork_tcpgrp
+			if(jobwasset)
+			{
+				signal(SIGTTIN,SIG_IGN);
+				signal(SIGTTOU,SIG_IGN);
+				if(sh_isstate(SH_INTERACTIVE))
+					signal(SIGTSTP,SIG_IGN);
+				else
+					signal(SIGTSTP,SIG_DFL);
+			}
+			if(job.jobcontrol)
+				tcsetpgrp(job.fd,sh.pid);
+#endif /* _use_ntfork_tcpgrp */
+			switch(errno=sh.path_err)
+			{
+			    case ENOENT:
+				errormsg(SH_DICT,ERROR_exit(ERROR_NOENT),e_found+4);
+				UNREACHABLE();
 #ifdef ENAMETOOLONG
-		    case ENAMETOOLONG:
-			errormsg(SH_DICT,ERROR_exit(ERROR_NOENT),e_toolong+4);
-			UNREACHABLE();
+			    case ENAMETOOLONG:
+				errormsg(SH_DICT,ERROR_exit(ERROR_NOENT),e_toolong+4);
+				UNREACHABLE();
 #endif
-		    default:
-			errormsg(SH_DICT,ERROR_system(ERROR_NOEXEC),e_exec+4);
-			UNREACHABLE();
+			    default:
+				errormsg(SH_DICT,ERROR_system(ERROR_NOEXEC),e_exec+4);
+				UNREACHABLE();
+			}
 		}
 		job_unlock();
 	}