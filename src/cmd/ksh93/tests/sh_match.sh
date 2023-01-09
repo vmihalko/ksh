edf23e3d86f37d41a605d68bef77d1e75f6ee1dc
Phi <phi.debian@gmail.com>
2023-01-09 07:39:09 +0000

    Fix bug #577 [^]] not working C.UTF8 (#581)
    Reproducer (in UTF-8 or ISO locale):
    
    $ ksh -c '[[ "[a] b [c] d" =~ ^\[[^]]+\] ]] && echo ${.sh.match}'
    [a] b [c]
    
    Expected output: [a]
    
    The output is already correct in the C locale.
    
    Analysis:
    
    In regconp()-->bra() i.e bracket handling, in the
    
        if (complicated && mbcoll())
        {
    
    ie (i.e UTF8) when handling ']' in a negative pattern class, i.e.,
    [^]...] the ']' is mishandled and then enters wrong elements in the
    collate table. It misleads regnexec() into not detecting the ']'.
    
    The original code
    
        else if (c == ']')
        { ....
    
    The fix
    
        else if (c == ']' && !(neg && first == pp))
        { ....
    
    If we are in the neg case [^ and if first is '[' and pp is at the
    first position don't do this. I then fall back into the normal
    processing, effectively entering the ']' on the negate class.
    
    Resolves: https://github.com/ksh93/ksh/issues/577
    Co-authored-by: Martijn Dekker <martijn@inlv.org>

--- src/cmd/ksh93/tests/sh_match.sh
+++ src/cmd/ksh93/tests/sh_match.sh
@@ -3,7 +3,7 @@
 #               This software is part of the ast package               #
 #          Copyright (c) 1982-2012 AT&T Intellectual Property          #
 #                    Copyright (c) 2012 Roland Mainz                   #
-#          Copyright (c) 2020-2022 Contributors to ksh 93u+m           #
+#          Copyright (c) 2020-2023 Contributors to ksh 93u+m           #
 #                      and is licensed under the                       #
 #                 Eclipse Public License, Version 2.0                  #
 #                                                                      #
@@ -15,6 +15,7 @@
 #                Roland Mainz <roland.mainz@nrubsig.org>               #
 #            Johnothan King <johnothanking@protonmail.com>             #
 #                  Martijn Dekker <martijn@inlv.org>                   #
+#                      Phi <phi.debian@gmail.com>                      #
 #                                                                      #
 ########################################################################
 
@@ -1063,6 +1064,13 @@ EOF
 	[[ $exp == "$got" ]] || err_exit "Compound variable \$context is not printed with 'print -v'." \
 		$'Diff follows:\n'"$(diff -u <(print -r -- "$exp") <(print -r -- "$got") | sed $'s/^/\t| /')"
 fi
+
+# ======
+
+# https://github.com/ksh93/ksh/issues/577
+# https://github.com/ksh93/ksh/pull/581
+[[ "[a] b [c] d" =~ ^\[[^]]+\] ]]
+[[ ${.sh.match} == '[a]' ]] || err_exit 'pattern ^\[[^]]+ broken'
 
 # ======
 exit $((Errors<125?Errors:125))
