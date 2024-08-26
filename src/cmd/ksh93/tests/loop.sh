vmihalko/ksh

commit f8f2c4b60835dd5d37a02f8dc0fe62ab74e4da94

Remove obsolete quote balancing hack
The old Bourne shell failed to check for closing quotes and command
substitution backticks when encountering end-of-file in a parser
context (such as a script). ksh93 implemented a hack for partial
compatibility with this bug, tolerating unbalanced quotes and
backticks in backtick command subsitutions, 'eval', and command
line invocation '-c' scripts only.

This hack became broken for backtick command substitutions in
fe20311f/350b52ea as a memory leak was fixed by adding a newline to
the stack at the end of the command substitution. That extra
newline becomes part of any string whose quotes are not properly
terminated, causing problems such as the one detailed here:
https://www.mail-archive.com/ast-developers@lists.research.att.com/msg01889.html

    $ touch abc
    $ echo `ls "abc`
    ls: abc
    : not found

No other fix for the memory leak is known that doesn't cause other
problems. (The alternative fix detailed in the referenced mailing
list post causes a different corner-case regression.)

Besides, the hack has always caused other corner case bugs as well:

	$ ksh -c '((i++'
Actual:	ksh: i++(: not found
	(If an external command 'i++(' existed, it would be run)
Expect:	ksh: syntax error at line 1: `(' unmatched

	$ ksh -c 'i=0; echo $((++i'
Actual:	(empty line; the arithmetic expansion is ignored)
Expect:	ksh: syntax error at line 1: `(' unmatched

	$ ksh -c 'echo $(echo "hi)'
Actual:	ksh: syntax error at line 1: `(' unmatched
Expect: ksh: syntax error at line 1: `"' unmatched

So, it's time to get rid of this hack. The old Bourne shell is
dead and buried. No other shell tries to support this breakage.
Tolerating syntax errors is just asking for strange side effects,
inconsistent states, and corner case bugs. We should not want to do
that. Old scripts that rely on this will just need to be fixed.

src/cmd/ksh93/sh/lex.c:
- struct lexdata: Remove 'char balance' member for remembering an
  unbalanced quote or backtick.

src/cmd/ksh93/sh/lex.c
@@ -92,7 +92,6 @@ struct lexdata
 	char		nested_tilde;
 	char 		*docend;
 	char		noarg;
-	char		balance;
 	char		warn;
 	char		message;
 	char		arith;
@@ -277,7 +276,7 @@ Lex_t *sh_lexopen(Lex_t *lp, Shell_t *sp, int mode)
 		lp->lexd.warn=1;
 	if(!mode)
 	{
-		lp->lexd.noarg = lp->lexd.level= lp->lexd.dolparen = lp->lexd.balance = 0;
+		lp->lexd.noarg = lp->lexd.level= lp->lexd.dolparen = 0;
 		lp->lexd.nocopy = lp->lexd.docword = lp->lexd.nest = lp->lexd.paren = 0;
 		lp->lexd.lex_state = lp->lexd.lastc=0;
 		lp->lexd.docend = 0;
@@ -326,7 +325,6 @@ int sh_lex(Lex_t* lp)
 	Stk_t			*stkp = shp->stk;
 	int		inlevel=lp->lexd.level, assignment=0, ingrave=0;
 	int		epatchar=0;
-	Sfio_t *sp;
 #if SHOPT_MULTIBYTE
 	LEN=1;
 #endif /* SHOPT_MULTIBYTE */
@@ -397,7 +395,6 @@ int sh_lex(Lex_t* lp)
 				fcseek(-LEN);
 				goto breakloop;
 			case S_EOF:
-				sp = fcfile();
 				if((n=lexfill(lp)) > 0)
 				{
 					fcseek(-1);
@@ -446,16 +443,11 @@ int sh_lex(Lex_t* lp)
 							c = LBRACE;
 							break;
 						case '"': case '`': case '\'':
-							lp->lexd.balance = c;
 							break;
 					}
-					if(sp && !(sfset(sp,0,0)&SF_STRING))
-					{
-						lp->lasttok = c;
-						lp->token = EOFSYM;
-						sh_syntax(lp);
-					}
-					lp->lexd.balance = c;
+					lp->lasttok = c;
+					lp->token = EOFSYM;
+					sh_syntax(lp);
 				}
 				goto breakloop;
 			case S_COM:
@@ -1299,13 +1291,9 @@ int sh_lex(Lex_t* lp)
 	}
 breakloop:
 	if(lp->lexd.nocopy)
-	{
-		lp->lexd.balance = 0;
 		return(0);
-	}
 	if(lp->lexd.dolparen)
 	{
-		lp->lexd.balance = 0;
 		if(lp->lexd.docword)
 			nested_here(lp);
 		lp->lexd.message = (wordflags&ARG_MESSAGE);
@@ -1318,12 +1306,6 @@ breakloop:
 		lp->arg = (struct argnod*)stkseek(stkp,ARGVAL);
 	if(n>0)
 		sfwrite(stkp,state,n);
-	/* add balancing character if necessary */
-	if(lp->lexd.balance)
-	{
-		sfputc(stkp,lp->lexd.balance);
-		lp->lexd.balance = 0;
-	}
 	sfputc(stkp,0);
 	stkseek(stkp,stktell(stkp)-1);
 	state = stkptr(stkp,ARGVAL);

src/cmd/ksh93/COMPATIBILITY
@@ -89,6 +89,9 @@ For more details, see the NEWS file and for complete details, see the git log.
 
 15.	'command -x' now always runs an external command, bypassing built-ins.
 
+16.	Unbalanced quotes and backticks now correctly produce a syntax error
+	in -c scripts, 'eval', and backtick-style command substitutions.
+
 ____________________________________________________________________________
 
 		KSH-93 VS. KSH-88

src/cmd/ksh93/include/version.h
@@ -20,7 +20,7 @@
 
 #define SH_RELEASE_FORK	"93u+m"		/* only change if you develop a new ksh93 fork */
 #define SH_RELEASE_SVER	"1.0.0-alpha"	/* semantic version number: https://semver.org */
-#define SH_RELEASE_DATE	"2021-03-04"	/* must be in this format for $((.sh.version)) */
+#define SH_RELEASE_DATE	"2021-03-05"	/* must be in this format for $((.sh.version)) */
 #define SH_RELEASE_CPYR	"(c) 2020-2021 Contributors to ksh " SH_RELEASE_FORK
 
 /* Scripts sometimes field-split ${.sh.version}, so don't change amount of whitespace. */

commit 1a30a27612bef8bc21f843874429d8f02f7dde59

Fix infinite loop on arithmetic for syntzx error on interactive
Marc Wilson (@posguy99) reports:

> A completely errant paste that wasn't meant for the terminal
> window exposed this...
>
> Reproducer:
>
>   $ for ((i = 5 , i = 0, --i))
>   -ksh: syntax error: `{' unmatched
>   -ksh: syntax error: `{' unmatched
>   -ksh: syntax error: `{' unmatched
>   -ksh: syntax error: `{' unmatched
>   -ksh: syntax error: `{' unmatched
>
> Continues forever until you kill the shell.

Simplest reproducer is 'for(())', with identical symptoms. In
scripts, the symptom is less severe, but the error message is still
incorrect as above. For example:

  $ ksh -c 'for(())'
  ksh: syntax error at line 1: `{' unmatched

The expected error is:

  ksh: syntax error at line 1: `))' unexpected

ksh93u+ and ksh2020 are also broken, just not in the same way, and
harder to reproduce. The current infinite-loop manifestation of the
bug was introduced in commit f8f2c4b.

Analysis: what happens is this:

1. arithfor() in parse.c is called to parse the arithmetic for loop
2. arithfor() saves current input stream and opens a new input
   stream to separate each of the three expressions in (( ... ))
3. on line 758, arithfor() calls sh_lexskip() to find the next ';'
4. in lex.c line 1749, sh_lexskip() calls the main lexer function,
   sh_lex()
5. in lex.c line 375, sh_lex() calls sh_syntax() to throw the
   syntax error in the reproducer
6. sh_syntax() calls errormsg() from libast, which causes a longjmp
   straight back to the prompt, without giving arithfor() a chance
   to restore the input stream that it saved in step 2. This is the
   bug. We can get all sorts of unpredictable/undefined behaviour
   that way.

I can think of two strategies to fix this:

1. Push context and sigsetjmp in arithfor() before calling
   sh_lexskip(), so that it longjmps back to arithfor() and it can
   restore the input stream before doing its own longjmp. This may

src/cmd/ksh93/include/shlex.h
@@ -52,7 +52,7 @@ struct _shlex_pvt_lexdata_
 	char		docword;
 	char		nested_tilde;
 	char 		*docend;
-	char		noarg;
+	char		inlexskip;	/* set when sh_lex() is called from sh_lexskip() */
 	char		warn;
 	char		message;
 	char		arith;

src/cmd/ksh93/sh/lex.c
@@ -150,13 +150,13 @@ static void lex_advance(Sfio_t *iop, const char *buff, int size, void *context)
 	{
 		size -= (lp->lexd.first-(char*)buff);
 		buff = lp->lexd.first;
-		if(!lp->lexd.noarg)
+		if(!lp->lexd.inlexskip)
 			lp->arg = stkseek(sh.stk,ARGVAL);
 #if SHOPT_KIA
 		lp->lexd.kiaoff += ARGVAL;
 #endif /* SHOPT_KIA */
 	}
-	if(size>0 && (lp->arg||lp->lexd.noarg))
+	if(size>0 && (lp->arg||lp->lexd.inlexskip))
 	{
 		sfwrite(sh.stk,buff,size);
 		lp->lexd.first = 0;
@@ -344,7 +344,7 @@ int sh_lex(Lex_t* lp)
 				/* end-of-file */
 				if(mode==ST_BEGIN)
 					return lp->token=EOFSYM;
-				if(mode >ST_NORM && lp->lexd.level>0)
+				if(mode >ST_NORM && lp->lexd.level>0 && !lp->lexd.inlexskip)
 				{
 					switch(c=endchar(lp))
 					{
@@ -706,7 +706,7 @@ int sh_lex(Lex_t* lp)
 				mode = ST_NORM;
 				continue;
 			case S_LIT:
-				if(oldmode(lp)==ST_NONE && !lp->lexd.noarg)	/* in ((...)) */
+				if(oldmode(lp)==ST_NONE && !lp->lexd.inlexskip)	/* in ((...)) */
 				{
 					if((c=fcpeek(0))==LPAREN || c==RPAREN || c=='$' || c==LBRACE || c==RBRACE || c=='[' || c==']')
 					{
@@ -1741,13 +1741,13 @@ void sh_lexskip(Lex_t *lp,int close, int copy, int  state)
 	char	*cp;
 	lp->lexd.nest = close;
 	lp->lexd.lex_state = state;
-	lp->lexd.noarg = 1;
+	lp->lexd.inlexskip = 1;
 	if(copy)
 		fcnotify(lex_advance,lp);
 	else
 		lp->lexd.nocopy++;
 	sh_lex(lp);
-	lp->lexd.noarg = 0;
+	lp->lexd.inlexskip = 0;
 	if(copy)
 	{
 		fcnotify(0,lp);

src/cmd/ksh93/tests/loop.sh
@@ -70,4 +70,16 @@ exp=a1xb1xc1x
 [[ $got == "$exp" ]] || err_exit "'continue 3' broken (expected '$exp', got '$got')"
 
 # ======
+# arithmetic for
+
+got=$(eval 'for((i=0,i<10,i++)); do :; done' 2>&1)
+exp=': syntax error at line 1: `))'\'' unexpected'
+[[ $got == *"$exp" ]] || err_exit "arithmetic for syntax error" \
+	"(expected match of *$(printf %q "$exp"), got $(printf %q "$got"))"
+got=$(eval 'for(())' 2>&1)
+exp=': syntax error at line 1: `))'\'' unexpected'
+[[ $got == *"$exp" ]] || err_exit "arithmetic for syntax error" \
+	"(expected match of *$(printf %q "$exp"), got $(printf %q "$got"))"
+
+# ======
 exit $((Errors<125?Errors:125))